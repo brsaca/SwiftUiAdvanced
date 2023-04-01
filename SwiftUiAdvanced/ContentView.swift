2bf97dcbb8818bc1a6e4589c251f30e80b2d4ef8
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-01 13:19:21 -0600

    feat: add password field

--- SwiftUiAdvanced/ContentView.swift
+++ SwiftUiAdvanced/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var email: String = ""
+    @State private var password: String = ""
 
     var body: some View {
         ZStack {
@@ -25,6 +26,7 @@ struct ContentView: View {
                     Text("Access to 120+ hours of courses, tutorias, and livestreams")
                         .font(.subheadline)
                         .foregroundColor(.white.opacity(0.7))
+                    
                     HStack(spacing: 12) {
                         Image(systemName: "envelope.open.fill").foregroundColor(.white)
                             .padding(.leading, 12)
@@ -32,6 +34,26 @@ struct ContentView: View {
                             .foregroundColor(.white.opacity(0.7))
                             .autocapitalization(.none)
                             .textContentType(.emailAddress)
+                    }
+                    .frame(height: 52)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(.white, lineWidth: 1.0)
+                            .blendMode(.overlay)
+                    )
+                    .background(
+                        Color("secondaryBackground")
+                            .cornerRadius(16.0)
+                            .opacity(0.8)
+                    )
+                    
+                    HStack(spacing: 12) {
+                        Image(systemName: "key.fill").foregroundColor(.white)
+                            .padding(.leading, 12)
+                        SecureField("Password", text: $password).colorScheme(.dark)
+                            .foregroundColor(.white.opacity(0.7))
+                            .autocapitalization(.none)
+                            .textContentType(.password)
                     }
                     .frame(height: 52)
                     .overlay(
