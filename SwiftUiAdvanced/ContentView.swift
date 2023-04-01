e140edacf682e5450f57442bfb7ff0412a50aeb0
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-01 13:07:45 -0600

    style: add assets and first view signUp

--- SwiftUiAdvanced/ContentView.swift
+++ SwiftUiAdvanced/ContentView.swift
@@ -6,80 +6,39 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+        ZStack {
+            Image("background-3")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Sign up")
+                        .font(.largeTitle.bold())
+                        .foregroundColor(.white)
+                    Text("Access to 120+ hours of courses, tutorias, and livestreams")
+                        .font(.subheadline)
+                        .foregroundColor(.white.opacity(0.7))
+                }.padding(20)
+            }.background(
+                RoundedRectangle(cornerRadius: 30)
+                    .stroke(.white.opacity(0.2))
+                    .background(Color("secondaryBackground").opacity(0.5))
+                    .background(VisualEffectBlur(blurStyle: .systemThinMaterialDark))
+                    .shadow(color: Color("shadowColor").opacity(0.5), radius: 60, x: 0, y:30)
+            ).cornerRadius(30)
+                .padding(.horizontal)
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+    
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
