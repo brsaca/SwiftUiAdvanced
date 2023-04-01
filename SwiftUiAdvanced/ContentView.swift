27d4268f39222fdcd25fcb14ffbd3c27e98ab902
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-01 13:53:02 -0600

    style: add gradient buttons

--- SwiftUiAdvanced/ContentView.swift
+++ SwiftUiAdvanced/ContentView.swift
@@ -67,6 +67,30 @@ struct ContentView: View {
                             .opacity(0.8)
                     )
                     
+                    GradientButton()
+                    
+                    Text("By clicking on Sign up, you agree to our Terms of service and Privacy policy")
+                        .font(.footnote)
+                        .foregroundColor(.white.opacity(0.7))
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(.white.opacity(0.1))
+                    
+                    VStack(alignment: .leading, spacing: 16) {
+                        Button(action: {
+                            print("Sign In")
+                        }, label: {
+                            HStack(spacing: 4) {
+                                Text("Already have an account?")
+                                    .font(.footnote)
+                                    .foregroundColor(.white.opacity(0.7))
+                                GradientText(text: "Sign in")
+                                    .font(.footnote)
+                                    .bold()
+                            }
+                        })
+                    }
+                    
                 }
                 .padding(20)
             }
@@ -89,5 +113,67 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
+
+extension View {
+    public func gradientForeground(colors:[Color]) -> some View {
+        self.overlay(LinearGradient(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .mask(self)
+    }
+}
+
+struct GradientText: View {
+    
+    var text: String = "Text here"
+    
+    var body: some View {
+        Text(text)
+            .gradientForeground(colors: [Color("pink-gradient-1"), Color("pink-gradient-2")])
+    }
+}
+
+struct GradientButton: View {
+    
+    var gradient1: [Color] = [
+        Color.init(red: 101/255, green: 134/255, blue: 1),
+        Color.init(red: 1, green: 64/255, blue: 80/255),
+        Color.init(red: 109/255, green: 1, blue: 185/255),
+        Color.init(red: 39/255, green: 232/255, blue: 1)
+    ]
+    
+    var body: some View {
+        Button(action: {
+            print("Sign up")
+        }, label: {
+            GeometryReader() { geometry in
+                ZStack {
+                    AngularGradient(colors: gradient1, center: .center, angle: .degrees(0))
+                        .blendMode(.overlay)
+                        .blur(radius: 8.0)
+                        .mask(
+                            RoundedRectangle(cornerRadius: 16.0)
+                                .frame(height: 50)
+                                .frame(maxWidth: geometry.size.width - 16)
+                                .blur(radius: 8.0)
+                        )
+                    GradientText(text: "Sign Up")
+                        .font(.headline)
+                        .frame(maxWidth: geometry.size.width - 16)
+                        .frame(height: 50)
+                        .background(
+                            Color("tertiaryBackground").opacity(0.9)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                                .stroke(.white, lineWidth: 1.9)
+                                .blendMode(.normal)
+                                .opacity(0.7)
+                        )
+                        .cornerRadius(16)
+                }
+            }
+            .frame(height: 50)
+        })
     }
 }
