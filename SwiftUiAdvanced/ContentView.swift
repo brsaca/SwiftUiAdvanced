b72cf6fdf40ce8cfa12e666a28f749c5e11c09a7
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-01 13:16:46 -0600

    feat: add email textfield

--- SwiftUiAdvanced/ContentView.swift
+++ SwiftUiAdvanced/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var email: String = ""
 
     var body: some View {
         ZStack {
@@ -24,15 +25,38 @@ struct ContentView: View {
                     Text("Access to 120+ hours of courses, tutorias, and livestreams")
                         .font(.subheadline)
                         .foregroundColor(.white.opacity(0.7))
-                }.padding(20)
-            }.background(
+                    HStack(spacing: 12) {
+                        Image(systemName: "envelope.open.fill").foregroundColor(.white)
+                            .padding(.leading, 12)
+                        TextField("Email", text: $email).colorScheme(.dark)
+                            .foregroundColor(.white.opacity(0.7))
+                            .autocapitalization(.none)
+                            .textContentType(.emailAddress)
+                    }
+                    .frame(height: 52)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(.white, lineWidth: 1.0)
+                            .blendMode(.overlay)
+                    )
+                    .background(
+                        Color("secondaryBackground")
+                            .cornerRadius(16.0)
+                            .opacity(0.8)
+                    )
+                    
+                }
+                .padding(20)
+            }
+            .background(
                 RoundedRectangle(cornerRadius: 30)
                     .stroke(.white.opacity(0.2))
                     .background(Color("secondaryBackground").opacity(0.5))
                     .background(VisualEffectBlur(blurStyle: .systemThinMaterialDark))
                     .shadow(color: Color("shadowColor").opacity(0.5), radius: 60, x: 0, y:30)
-            ).cornerRadius(30)
-                .padding(.horizontal)
+            )
+            .cornerRadius(30)
+            .padding(.horizontal)
         }
     }
 
