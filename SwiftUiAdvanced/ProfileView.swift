e7cb5aac77c7345e69f30b51cd2440f6776feebe
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-04 09:18:44 -0600

    refactor: rename contentview

---
+++ SwiftUiAdvanced/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  SwiftUiAdvanced
+//
+//  Created by Brenda Saavedra  on 04/04/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
