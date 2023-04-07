0ee2be6b51d08f89b82de81dbfe20e97666a2e38
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-07 11:15:07 -0600

    style: populate settings screen

--- SwiftUiAdvanced/SettingsView.swift
+++ SwiftUiAdvanced/SettingsView.swift
@@ -26,7 +26,72 @@ struct SettingsView: View {
     private let generator = UISelectionFeedbackGenerator()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack (alignment: .leading, spacing: 16) {
+                Spacer().frame(height: 50)
+                
+                Text("Settings")
+                    .foregroundColor(.white)
+                    .font(.largeTitle.bold())
+                    .padding(.top)
+                Text("Manage your Desing+Code profile and account")
+                    .foregroundColor(.white.opacity(0.7))
+                    .font(.callout)
+                
+                // Choose Photo
+                Button {
+                    
+                } label: {
+                    HStack(spacing: 12) {
+                        TextFieldIcon(iconName: "person.crop.circle", currentlyEditing: .constant(false))
+                        GradientText(text: "Choose Photo")
+                        Spacer()
+                    }
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(Color.white.opacity(0.1), lineWidth: 1)
+                    )
+                    .background(
+                        Color.init(red: 26/255, green: 20/255, blue: 51/255)
+                            .cornerRadius(16)
+                    )
+                }
+                
+                //Name textfield
+                GradientTextfield(editingTextfield: $editingNameTextfield, textfieldString: $name, iconBounce: $nameIconBounce, textfieldPlaceholder: "Name", textfieldIconString: "textformat.alt")
+                    .autocapitalization(.words)
+                    .textContentType(.name)
+                    .disableAutocorrection(true)
+                
+                //Twitter textfield
+                GradientTextfield(editingTextfield: $editingTwitterTextfield, textfieldString: $twitter, iconBounce: $twitterIconBounce, textfieldPlaceholder: "Twitter", textfieldIconString: "at")
+                    .autocapitalization(.none)
+                    .keyboardType(.twitter)
+                    .disableAutocorrection(true)
+                
+                //Site textfield
+                GradientTextfield(editingTextfield: $editingSiteTextfield, textfieldString: $site, iconBounce: $siteIconBounce, textfieldPlaceholder: "Website", textfieldIconString: "link")
+                    .autocapitalization(.none)
+                    .keyboardType(.webSearch)
+                    .disableAutocorrection(true)
+                
+                //Bio textfield
+                GradientTextfield(editingTextfield: $editingBioTextfield, textfieldString: $bio, iconBounce: $bioIconBounce, textfieldPlaceholder: "Bio", textfieldIconString: "text.justifyleft")
+                    .autocapitalization(.sentences)
+                    .keyboardType(.default)
+                
+                GradientButton(buttonTitle: "Save Settings", buttonAction: {
+                    generator.selectionChanged()
+                })
+                
+                Spacer()
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .background(Color("settingsBackground"))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
